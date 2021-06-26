7c0608b5992f18510fb6e53897f13da98530f8e2
Ash Barsov <a89875239469@gmail.com>
2021-06-26 15:47:45 +0300

    Updates

---
+++ precache-manifest.813544abeea5f884953f501d95d6834d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a253c7cd410fdab1dd6e5b9ae442f9ff",
+    "url": "/ToDoListFirstProject/index.html"
+  },
+  {
+    "revision": "6cdc385bad24e8517e6e",
+    "url": "/ToDoListFirstProject/static/css/main.41e11f77.chunk.css"
+  },
+  {
+    "revision": "29ca513d8ac6c5521bd3",
+    "url": "/ToDoListFirstProject/static/js/2.7c4d31cb.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoListFirstProject/static/js/2.7c4d31cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdc385bad24e8517e6e",
+    "url": "/ToDoListFirstProject/static/js/main.a64a4a15.chunk.js"
+  },
+  {
+    "revision": "1964e1c9b51c92c25437",
+    "url": "/ToDoListFirstProject/static/js/runtime-main.d2869f15.js"
+  }
+]);
